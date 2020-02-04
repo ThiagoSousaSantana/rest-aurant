4757b9cc67c482216091e34ed8e17f8131d4d115
Thiago <thiago91098@gmail.com>
2020-02-04 19:45:54 -0300

    API Gateway service created

---
+++ zuul-service/src/test/java/br/com/thiagosousa/zuulservice/ZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiagosousa.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
